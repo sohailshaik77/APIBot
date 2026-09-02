dec829c5b8c139cd45234fd943988742ec4ed30e
sohailshaik77 <47217147+sohailshaik77@users.noreply.github.com>
2026-09-02 16:47:40 +0530

    added testng.xml file

--- src/test/java/com/api/LocationApi.java
+++ src/test/java/com/api/LocationApi.java
@@ -1,12 +1,34 @@
 package com.api;
-import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class LocationApi {
 
     @Test
     public void getLocation(){
+        Response response = given()
+                .queryParam("q", "Hyderabad")
+                .queryParam("format", "jsonv2")
+                .header("User-Agent", "RestAssured-Test")
+                .when()
+                .get("https://nominatim.openstreetmap.org/search")
+                .then()
+                .extract()
+                .response();
+
+        response.prettyPrint();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        String displayName =
+                response.jsonPath().getString("[0].display_name");
+
+        System.out.println("Location: " + displayName);
+
+        Assert.assertTrue(displayName.contains("Hyderabad"));
 
     }
 }
